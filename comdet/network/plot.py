71f93f77714c4a80be798ad289883008e90e02af
marianotepper <Lulita75>
2015-11-18 18:06:23 -0500

    Fixed colors in the preference matrix

--- comdet/network/plot.py
+++ comdet/network/plot.py
@@ -107,12 +107,11 @@ def draw(graph, communities, pos=None, output=None):
                            output=output)
 
 
-
 def plot_preference_matrix(array, samples, offset=0, labels=None, title=None):
     n_colors = int(np.max(array))
     palette = sns.cubehelix_palette(n_colors + 1, start=2, rot=0, dark=0.15,
                                     light=1)
-    cmap = colors.ListedColormap(palette, N=n_colors )
+    cmap = colors.ListedColormap(palette, N=n_colors + 1)
     plt.imshow(array, interpolation='none', cmap=cmap)
     count = 0
     for neigh_comm in samples:

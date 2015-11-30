Fixed radius computation in circle

comdet/pme/circle.py
@@ -19,16 +19,11 @@ class Circle(object):
         if data.shape[0] < 3:
             raise ValueError('At least three points are needed to fit a circle')
 
-        M = np.ones((data.shape[0], 3))
-        b = np.zeros((data.shape[0],))
-
-        for j in range(data.shape[0]):
-            M[j, :2] = data[j, :]
-            b[j] = - np.sum(data[j, :] ** 2)
-
-        y = np.linalg.solve(M, b)
-        self.center = -y[:2] / 2
-        self.radius = np.linalg.norm(self.center) - y[2]
+        a = np.hstack((data, np.ones((data.shape[0], 1))))
+        b = -np.sum(data ** 2, axis=1)
+        y = np.linalg.lstsq(a, b)[0]
+        self.center = -0.5 * y[:2]
+        self.radius = np.sqrt(np.sum(self.center ** 2) - y[2])
 
     def distances(self, data):
         return np.abs(np.linalg.norm(data - self.center, axis=1) - self.radius)
@@ -37,14 +32,19 @@ class Circle(object):
         t = np.arange(0, 2*np.pi + 0.1, 0.1)
         x = self.center[0] + self.radius * np.cos(t)
         y = self.center[1] + self.radius * np.sin(t)
+        plt.hold(True)
+        plt.scatter(self.center[0], self.center[1])
         plt.plot(x, y, **kwargs)
 
 
 def test():
-    x = np.array([[1, 1], [1, 2], [0, 0]])
+    x = np.array([[-0.08045062, 1.42318932],
+                  [ 0.33886852, 1.16171235],
+                  [-0.21174648, 1.08196667]])
+    # x = np.array([[1, 1], [1, 2], [0, 0]])
     c1 = Circle(x)
     print c1.distances(x)
-    print c1.distances(np.array([[0.5, 0], [0, 1], [0, 2]]))
+    # print c1.distances(np.array([[0.5, 0], [0, 1], [0, 2]]))
     # x = np.array([[0, 0], [2, 2]])
     # l2 = Line(x)
     # print l2.distances(x)
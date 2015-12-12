9e2daa0f903d56ab772fccdd18415703b4dd743b
marianotepper <Lulita75>
2015-12-12 17:53:02 -0500

    Added projection

--- comdet/pme/line.py
+++ comdet/pme/line.py
@@ -31,6 +31,17 @@ class Line(object):
             data = np.hstack((data, np.ones((data.shape[0], 1))))
         return np.abs(np.dot(data, self.eq)) / np.linalg.norm(self.eq[:2])
 
+    def project(self, data):
+        u = np.array([self.eq[1], -self.eq[0]])
+        if abs(self.eq[1]) > abs(self.eq[0]):
+            x0 = np.array([0, -self.eq[2] / self.eq[1]])
+        else:
+            x0 = np.array([-self.eq[2] / self.eq[0], 0])
+        u /= np.linalg.norm(u)
+        s = np.dot(data - x0, u)
+        proj = x0 + np.atleast_2d(s).T * u
+        return proj, s, u, x0
+
     def plot(self, limits=None, **kwargs):
         if limits is None:
             xlim = plt.xlim()
@@ -42,7 +53,7 @@ class Line(object):
         if abs(self.eq[0]) > abs(self.eq[1]):  # line is more vertical
             p1 = np.array([0., 1., -ylim[0]])
             p2 = np.array([0., 1., -ylim[1]])
-        else:  # line more horizontal
+        else:  # line is more horizontal
             p1 = np.array([1., 0., -xlim[0]])
             p2 = np.array([1., 0., -xlim[1]])
 
@@ -63,9 +74,44 @@ def _test():
     print l2.distances(x)
 
     plt.figure()
+    plt.scatter(x[:, 0], x[:, 1])
+    # plt.scatter([l1.eq[1]], [-l1.eq[0] - l1.eq[2] / l1.eq[1]], c='r', s=50)
+    plt.scatter([l1.eq[1] - l1.eq[2] / l1.eq[0]], [l1.eq[0]], c='r', s=50)
+    plt.scatter([l2.eq[1] - l2.eq[2] / l2.eq[0]], [-l2.eq[0]], c='r', s=50)
     l1.plot(color='g', linewidth=2)
     l2.plot(color='g', linewidth=2)
+    plt.axis('equal')
     plt.show()
 
+
+def _test2():
+    import scipy.io
+    examples = scipy.io.loadmat('../data/JLinkageExamples.mat')
+    data = examples['Star5_S0015_O0'].T
+
+
+    np.random.seed(0)
+    samples = np.random.randint(data.shape[0], size=2)
+    print samples
+    l1 = Line(data[samples, :])
+
+    plt.figure()
+    proj, s = l1.project(data)
+    print proj.shape
+    print np.min(s), np.max(s)
+
+
+    plt.scatter(data[:, 0], data[:, 1], c='w')
+    l1.plot(color='r')
+    plt.plot([data[:, 0], proj[:, 0]], [data[:, 1], proj[:, 1]], color='b', alpha=0.2)
+    plt.scatter(data[samples, 0], data[samples, 1], c='r')
+    plt.scatter([0], [0], c='k')
+    plt.axis('equal')
+
+
+
+    plt.show()
+
+
 if __name__ == '__main__':
-    _test()
+    _test2()

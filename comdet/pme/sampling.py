6e0218061805724ae103f75974f736a7cac52499
marianotepper <Lulita75>
2015-12-19 21:10:46 -0500

    -epsilon

--- comdet/pme/sampling.py
+++ comdet/pme/sampling.py
@@ -4,7 +4,7 @@ import scipy.spatial.distance as distance
 import itertools
 
 
-class UniformSampler(object):
+class SimpleSampler(object):
     def __init__(self, n_samples=None):
         self.n_samples = n_samples
 
@@ -18,25 +18,47 @@ class UniformSampler(object):
             yield sample
 
 
+class UniformSampler(object):
+    def __init__(self, n_samples=None):
+        self.n_samples = n_samples
+
+    def generate(self, x, min_sample_size):
+        n_elements = len(x)
+        distribution = np.zeros((n_elements,))
+        for _ in range(self.n_samples):
+            bins = np.cumsum(distribution.max() - distribution)
+            if bins[-1] > 0:
+                rnd = np.random.randint(0, bins[-1], size=min_sample_size)
+                sample = np.searchsorted(bins, rnd)
+            else:
+                sample = np.array([], dtype=np.int)
+            unique_sample = np.unique(sample)
+            if unique_sample.size < min_sample_size:
+                remainder = min_sample_size - unique_sample.size
+                comp = np.random.randint(0, n_elements, size=remainder)
+                sample = np.append(sample, [comp])
+            distribution[sample] += 1
+            yield sample
+
+
 class GaussianLocalSampler(object):
     def __init__(self, sigma, n_samples=None):
         self.n_samples = n_samples
         # p(x[i] | x[j]) = exp(-(dist(x[i], x[j])) / sigma)
         self.var = sigma ** 2
-        self.distribution = None
 
     def generate(self, x, min_sample_size):
         n_elements = len(x)
-        self.distribution = np.zeros((n_elements,))
+        distribution = np.zeros((n_elements,))
         counter_samples = 0
         counter_total = 0
         while (counter_samples < self.n_samples and
-                       counter_total < self.n_samples * 100):
-            bins = np.cumsum(self.distribution.max() - self.distribution)
+               counter_total < self.n_samples * 100):
+            bins = np.cumsum(distribution.max() - distribution)
             bins /= bins[-1]
             rnd = np.random.random()
             j = np.searchsorted(bins, rnd)
-            dists = distance.cdist(x, np.atleast_2d(x[j, :]), 'euclidean')
+            dists = distance.cdist(x, np.atleast_2d(x[j]), 'euclidean')
             bins = np.cumsum(np.exp(-(dists ** 2) / self.var))
             bins /= bins[-1]
 
@@ -44,13 +66,13 @@ class GaussianLocalSampler(object):
             for _ in range(100):
                 rnd = np.random.random((min_sample_size - 1,))
                 sample = np.searchsorted(bins, rnd)
-                sample = np.hstack((sample, [j]))
+                sample = np.append(sample, [j])
                 if np.unique(sample).size == min_sample_size:
                     success = True
                     break
 
             if success:
-                self.distribution[sample] += 1
+                distribution[sample] += 1
                 counter_samples += 1
                 yield sample
             counter_total += 1

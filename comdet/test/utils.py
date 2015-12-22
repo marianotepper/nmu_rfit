bcbdb2c1882b9c2a43730f01b6a78edf3b8e21f0
marianotepper <Lulita75>
2015-12-22 16:35:02 -0500

    Added Logger class for redirecting stdout

--- comdet/test/utils.py
+++ comdet/test/utils.py
@@ -1,20 +1,8 @@
 from __future__ import absolute_import
+import sys
 import numpy as np
-import comdet.biclustering.preference as pref
 import comdet.pme.acontrario as ac
 import comdet.pme.measures as mes
-
-
-def build_preference_matrix(n_elements, ransac_gen, ac_tester):
-    filtered = ac.ifilter(ac_tester, ransac_gen)
-
-    pref_matrix = pref.create_preference_matrix(n_elements)
-    original_models = []
-    for i, (model, inliers) in enumerate(filtered):
-        pref_matrix = pref.add_col(pref_matrix, inliers)
-        original_models.append(model)
-
-    return pref_matrix, original_models
 
 
 def compute_measures(gt_groups, left_factors):
@@ -28,6 +16,7 @@ def clean(model_class, x, ac_tester, bic_list, restimate=True):
     bic_list = [bic for bic in bic_list
                 if bic[1].nnz > 1 and
                 bic[0].nnz > model_class().min_sample_size]
+
     mod_inliers_list = []
     for rf, _ in bic_list:
         inliers = np.squeeze(rf.toarray())
@@ -41,3 +30,16 @@ def clean(model_class, x, ac_tester, bic_list, restimate=True):
     mod_inliers_list = [mod_inliers_list[s] for s in survivors]
     bic_list = [bic_list[s] for s in survivors]
     return mod_inliers_list, bic_list
+
+
+class Logger(object):
+    def __init__(self, filename="Console.log"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def __getattr__(self, attr):
+        return getattr(self.terminal, attr)

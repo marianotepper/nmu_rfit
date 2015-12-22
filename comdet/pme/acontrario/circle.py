68c348909d8d1d2d8af51e35ac3c19371beed51c
marianotepper <Lulita75>
2015-12-22 16:44:32 -0500

    Fixed aberrant case in circle threshold

--- comdet/pme/acontrario/circle.py
+++ comdet/pme/acontrario/circle.py
@@ -11,40 +11,33 @@ class GlobalNFA(utils.BinomialNFA):
     def threshold(self, model):
         return self.inliers_threshold
 
-    def _random_probability(self, model, data=None, inliers_threshold=None):
-        if inliers_threshold is None:
-            inliers_threshold = self.inliers_threshold
+    def _total_and_probability(self, model, data, inliers_threshold):
         # (a + b)**2 - (a - b)**2 == 4ab
         ring_area = np.pi * 4 * model.radius * inliers_threshold
-        return min(ring_area / self.area, 1)
+        return len(data), min(ring_area / self.area, 1)
 
 
-class LocalNFA(object):
+class LocalNFA(utils.BinomialNFA):
     def __init__(self, data, epsilon, inliers_threshold):
-        self.data = data
-        self.epsilon = epsilon
+        super(LocalNFA, self).__init__(data, epsilon)
         self.inliers_threshold = inliers_threshold
 
-    def nfa(self, model, n_inliers, data=None, inliers_threshold=None):
-        if data is None:
-            data = self.data
-        if inliers_threshold is None:
-            inliers_threshold = self.inliers_threshold
+    def _total_and_probability(self, model, data, inliers_threshold):
+        if model.radius <= inliers_threshold:
+            raise ValueError('Inliers threshold too big for this circle')
 
-        if model.radius < inliers_threshold:
-            return np.inf
+        dist = model.distances(data)
+        upper_threshold = np.maximum(inliers_threshold * 3,
+                                     np.min(dist[dist > inliers_threshold]))
+        region_mask = dist <= upper_threshold
 
-        upper_threshold = inliers_threshold * 3.
-        region_mask = model.distances(data) <= upper_threshold
+        if model.radius < upper_threshold:
+            p = 4 * model.radius * inliers_threshold
+            p /= (model.radius + upper_threshold) ** 2
+        else:
+            p = inliers_threshold / upper_threshold
 
-        p = inliers_threshold / upper_threshold
-        k = n_inliers - model.min_sample_size
-        pfa = utils.log_binomial(region_mask.sum(), k, p)
-        n_tests = utils.log_nchoosek(data.shape[0], model.min_sample_size)
-        return (pfa + n_tests) / np.log(10)
-
-    def meaningful(self, model, n_inliers):
-        return self.nfa(model, n_inliers) < self.epsilon
+        return region_mask.sum(), p
 
     def threshold(self, model):
         return self.inliers_threshold

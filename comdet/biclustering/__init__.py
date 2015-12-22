12775adf68a2ca6879bc548213c50d478de3601e
marianotepper <Lulita75>
2015-12-22 16:31:24 -0500

    Moved module to another package

--- comdet/biclustering/__init__.py
+++ comdet/biclustering/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 from .nmf import bicluster
 from . import deflation
-from . import preference
 from . import utils
 
-__all__ = ['bicluster', 'deflation', 'preference', 'utils']
+__all__ = ['bicluster', 'deflation', 'utils']

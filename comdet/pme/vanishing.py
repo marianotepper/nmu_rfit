6248f86d36d8c09d4057b04919dccaafabcaef3a
marianotepper <Lulita75>
2015-12-28 15:14:13 -0500

    Fixed VP estimation from multiple lines

--- comdet/pme/vanishing.py
+++ comdet/pme/vanishing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.optimize
 import matplotlib.pyplot as plt
 
 
@@ -15,45 +16,29 @@ class VanishingPoint():
     def fit(self, data):
         if len(data) < self.min_sample_size:
             raise ValueError('At least two segments are needed to fit a VP')
-        lines = np.array([seg.line for seg in data])
-        sol = np.linalg.lstsq(lines[:, :2], -lines[:, 2])
-        if sol[2] == 2:  # matrix rank
-            self.point = np.append(sol[0], [1])
+        if len(data) == self.min_sample_size:
+            self.point = np.cross(data[0].line, data[1].line)
+            self.point /= self.point[2]
         else:
+            lines = np.array([seg.line for seg in data])
+            sol = np.linalg.lstsq(lines[:, :2], -lines[:, 2])
+            point_plane = np.append(sol[0], [1])
+            dists_plane = distances(point_plane, data)
+            print np.max(np.abs(dists_plane))
+
             angles = _normalize(np.arctan2(lines[:, 0], lines[:, 1]))
             alpha = np.mean(angles)
-            self.point = np.array([np.cos(alpha), np.sin(alpha), 0])
+            point_inf = np.array([np.cos(alpha), np.sin(alpha), 0])
+            dists_inf = distances(point_inf, data)
+            print np.max(np.abs(dists_inf))
 
-    # noinspection PyMethodMayBeStatic
-    def project(self, data):
-        lines = np.array([seg.line for seg in data])
-        if self.point[2] != 0:
-            return np.arctan2(lines[:, 1], lines[:, 0])
-        else:
-            return _normalize(np.arctan2(lines[:, 0], lines[:, 1]))
+            if np.max(np.abs(dists_plane)) < np.max(np.abs(dists_inf)):
+                self.point = point_plane
+            else:
+                self.point = point_inf
 
     def distances(self, data):
-        lines = np.array([seg.line for seg in data])
-        if self.point[2] != 0:
-            points_a, points_b = zip(*[(s.p_a, s.p_b) for s in data])
-            points_a = np.array(points_a)
-            points_b = np.array(points_b)
-            d_a = np.linalg.norm(points_a - self.point, axis=1)
-            d_b = np.linalg.norm(points_b - self.point, axis=1)
-            closer_points = np.copy(points_a)
-            closer_points[d_a > d_b] = points_b[d_a > d_b]
-            midpoints = (points_a + points_b) / 2
-            v1 = self.point - midpoints
-            v2 = closer_points - midpoints
-            diff = np.sum(v1 * v2, axis=1)
-            diff /= (np.linalg.norm(v1, axis=1) * np.linalg.norm(v2, axis=1))
-            ang_diff = np.arccos(diff) * np.sign(np.cross(v1, v2)[:, 2])
-            return ang_diff
-        else:
-            angleVP = _normalize(np.arctan2(self.point[1], self.point[0]))
-            angleL = _normalize(np.arctan2(lines[:, 0], lines[:, 1]))
-            angle_diff = angleVP - angleL
-            return angle_diff
+        return distances(self.point, data)
 
     def plot(self, **kwargs):
         plt.scatter(self.point[0], self.point[1], **kwargs)
@@ -61,3 +46,37 @@ class VanishingPoint():
 
 def _normalize(a):
     return np.fmod(a + np.pi, np.pi)
+
+
+def basis_vector(segment):
+    u = segment.p_a - segment.p_b
+    u /= np.linalg.norm(u)
+    return np.atleast_2d(u[:2])
+
+
+def distances(point, data):
+    lines = np.array([seg.line for seg in data])
+    if point[2] != 0:
+        points_a, points_b = zip(*[(s.p_a, s.p_b) for s in data])
+        points_a = np.array(points_a)
+        points_b = np.array(points_b)
+        d_a = np.linalg.norm(points_a - point, axis=1)
+        d_b = np.linalg.norm(points_b - point, axis=1)
+        closer_points = np.copy(points_a)
+        closer_points[d_a > d_b] = points_b[d_a > d_b]
+        midpoints = (points_a + points_b) / 2
+        v1 = point - midpoints
+        v2 = closer_points - midpoints
+        diff = np.sum(v1 * v2, axis=1)  # dot product
+        diff /= (np.linalg.norm(v1, axis=1) * np.linalg.norm(v2, axis=1))
+        ang_diff = np.arccos(diff) * np.sign(np.cross(v1, v2)[:, 2])
+        return ang_diff
+    else:
+        angle_vp = _normalize(np.arctan2(point[1], point[0]))
+        angle_lines = _normalize(np.arctan2(lines[:, 0], lines[:, 1]))
+        angle_diff = angle_vp - angle_lines
+        mask = angle_diff > np.pi / 2
+        angle_diff[mask] = np.pi - angle_diff[mask]
+        mask = angle_diff < -np.pi / 2
+        angle_diff[mask] = np.pi + angle_diff[mask]
+        return angle_diff

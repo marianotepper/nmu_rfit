4c80c8e553b2dc6f75a7b77afaa56a745f88188b
marianotepper <Lulita75>
2016-01-28 16:18:35 -0500

    Refactoring

--- comdet/biclustering/bicluster.py
+++ comdet/biclustering/bicluster.py
@@ -4,35 +4,7 @@ from . import utils
 from . import nmf
 from . import mdl
 from . import deflation
-
-
-def single_bicluster(deflator):
-    try:
-        u, v = nmf.nmf_robust_multiplicative(deflator.compressed_array, 1)
-        # u, v = nmf.nmf_robust_rank1(deflator.array_compressed)
-        u = utils.binarize(u)
-        idx_u = utils.find(u)[0]
-
-        array_cropped = deflator.array[idx_u, :]
-        u_cropped = utils.sparse(np.ones((idx_u.size, 1)))
-        v_init = u_cropped.T.dot(array_cropped)
-        v_init /= v_init.max()
-        v = nmf.nmf_robust_admm_v(array_cropped, u_cropped, v_init)
-        v = utils.binarize(v)
-        idx_v = utils.find(v)[1]
-
-        array_cropped = deflator.array[:, idx_v]
-        v_cropped = utils.sparse(np.ones((1, idx_v.size)))
-        u_init = array_cropped.dot(v_cropped.T)
-        u_init /= u_init.max()
-        u = nmf.nmf_robust_admm_u(array_cropped, u_init, v_cropped)
-        u = utils.binarize(u)
-
-    except deflation.DeflationError:
-        u, v = nmf.nmf_robust_admm(deflator.array)
-        u = utils.binarize(u)
-        v = utils.binarize(v)
-    return u, v
+import timeit
 
 
 def bicluster(deflator, n=None, share_elements=True):
@@ -74,3 +46,49 @@ def bicluster(deflator, n=None, share_elements=True):
         return bic_list[:cut_point+1]
     else:
         return bic_list
+
+
+def single_bicluster(deflator):
+    try:
+        u, v = nmf.nmf_robust_multiplicative(deflator.compressed_array, 1)
+        # u, v = nmf.nmf_robust_admm(deflator.compressed_array)
+        u = utils.binarize(u)
+        idx_u, _, vals_u = utils.find(u)
+
+        array_cropped, u_cropped, v_init = crop_left(deflator.array, idx_u)
+        v = nmf.nmf_robust_admm_v(array_cropped, u_cropped, v_init)
+        # _, v = nmf.nmf_robust_multiplicative(array_cropped, 1,
+        #                                      u_init=u_cropped,
+        #                                      v_init=v_init, update='right')
+        v = utils.binarize(v)
+        idx_v = utils.find(v)[1]
+
+        array_cropped, u_init, v_cropped = crop_right(deflator.array, idx_v)
+        u = nmf.nmf_robust_admm_u(array_cropped, u_init, v_cropped)
+        # u, _ = nmf.nmf_robust_multiplicative(array_cropped, 1, u_init=u_init,
+        #                                      v_init=v_cropped, update='left')
+        u = utils.binarize(u)
+
+    except deflation.DeflationError:
+        u, v = nmf.nmf_robust_admm(deflator.array)
+        u = utils.binarize(u)
+        v = utils.binarize(v)
+    return u, v
+
+
+def crop_left(array, idx_u):
+    array_cropped = utils.sparse(array[idx_u, :])
+    u_cropped = utils.sparse(np.ones((idx_u.size, 1)))
+    v_init = u_cropped.T.dot(array_cropped)
+    arr_sum = array_cropped.sum(axis=0)
+    v_init[arr_sum > 0] /= arr_sum[arr_sum > 0]
+    return array_cropped, u_cropped, v_init
+
+
+def crop_right(array, idx_v):
+    array_cropped = utils.sparse(array[:, idx_v])
+    v_cropped = utils.sparse(np.ones((1, idx_v.size)))
+    u_init = array_cropped.dot(v_cropped.T)
+    arr_sum = array_cropped.sum(axis=1)
+    u_init[arr_sum > 0] /= arr_sum[arr_sum > 0]
+    return array_cropped, u_init, v_cropped

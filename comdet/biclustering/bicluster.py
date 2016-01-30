9e8f6590f45a28372db193c09b9c8ab37f7f01f7
marianotepper <Lulita75>
2016-01-30 18:06:52 -0500

    Code refactoring to decouple MDL from biclustering

--- comdet/biclustering/bicluster.py
+++ comdet/biclustering/bicluster.py
@@ -7,15 +7,13 @@ from . import deflation
 
 
 def bicluster(deflator, n=None, share_elements=True):
-    bic_list = []
-    total_codelength = []
-
     if n is None:
         n_iters = deflator.array.shape[1]
-        online_mdl = mdl.OnlineMDL()
+        array_copy = deflator.array.copy()
     else:
         n_iters = n
 
+    bic_list = []
     for _ in range(n_iters):
         if deflator.array.nnz == 0:
             break
@@ -33,18 +31,13 @@ def bicluster(deflator, n=None, share_elements=True):
             idx_u = utils.find(u)[0]
             deflator.remove_rows(idx_u)
 
-        if n is None:
-            cl = online_mdl.add_rank1_approximation(deflator.array, u, v)
-            total_codelength.append(cl)
-
     if not bic_list:
         return bic_list
+
     if n is None:
-        total_codelength = np.array(total_codelength)
-        cut_point = np.argmin(total_codelength)
-        return bic_list[:cut_point+1]
-    else:
-        return bic_list
+        bic_list = mdl_cleanup(array_copy, bic_list, share_elements)
+
+    return bic_list
 
 
 def single_bicluster(deflator):
@@ -54,12 +47,14 @@ def single_bicluster(deflator):
         idx_u, _, vals_u = utils.find(u)
 
         array_crop, u_crop, v_init = crop_left(deflator.array, idx_u)
-        v = nmf.nmf_robust_admm(array_crop, u_crop, v_init, update='right')
+        v = nmf.nmf_robust_admm(array_crop, u_init=u_crop, v_init=v_init,
+                                update='right')
         v = utils.binarize(v)
         idx_v = utils.find(v)[1]
 
         array_crop, u_init, v_crop = crop_right(deflator.array, idx_v)
-        u = nmf.nmf_robust_admm_u(array_crop, u_init, v_crop, update='left')
+        u = nmf.nmf_robust_admm(array_crop, u_init=u_init, v_init=v_crop,
+                                update='left')
         u = utils.binarize(u)
 
     except deflation.DeflationError:
@@ -85,3 +80,25 @@ def crop_right(array, idx_v):
     arr_sum = array_cropped.sum(axis=1)
     u_init[arr_sum > 0] /= arr_sum[arr_sum > 0]
     return array_cropped, u_init, v_cropped
+
+
+def mdl_cleanup(array, bic_list, share_elements):
+    bic_list = sorted(bic_list, key=lambda bic: bic[0].nnz * bic[1].nnz,
+                      reverse=True)
+
+    mdl_deflator = deflation.Deflator(array)
+    online_mdl = mdl.OnlineMDL()
+
+    total_codelength = []
+    for u, v in bic_list:
+        idx_v = utils.find(v)[1]
+        mdl_deflator.remove_columns(idx_v)
+        if not share_elements:
+            idx_u = utils.find(u)[0]
+            mdl_deflator.remove_rows(idx_u)
+
+        cl = online_mdl.add_rank1_approximation(mdl_deflator.array, u, v)
+        total_codelength.append(cl)
+
+    cut_point = np.argmin(np.array(total_codelength))
+    return bic_list[:cut_point+1]

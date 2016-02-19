03c69336e9f9bce47c1e6744b8f3dd774e67f0df
marianotepper <Lulita75>
2016-02-19 07:15:56 -0500

    Code cleanup

--- arse/pme/homography.py
+++ arse/pme/homography.py
@@ -21,21 +21,14 @@ class Homography(object):
         if data.shape[1] != 6:
             raise ValueError('Points must be 6D (2 x 3D)')
 
-        # self.H = cv2.findHomography(data[:, 0:2].astype(np.float32),
-        #                             data[:, 3:5].astype(np.float32))
         pts1_norm, self._trans1 = utils.normalize_2d(data[:, :3])
         pts2_norm, self._trans2 = utils.normalize_2d(data[:, 3:])
         self.H, _ = cv2.findHomography(pts1_norm[:, 0:2].astype(np.float32),
                                        pts2_norm[:, 0:2].astype(np.float32))
 
     def distances(self, data):
-        # pts1_norm = data[:, :3]
-        # pts2_norm = data[:, 3:]
         pts1_norm = data[:, :3].dot(self._trans1)
         pts2_norm = data[:, 3:].dot(self._trans2)
         trans = np.dot(pts1_norm, self.H.T)
         trans /= np.atleast_2d(trans[:, 2]).T
-        # x = np.linalg.norm(data[:, :3] - trans.dot(self._trans2.T), axis=1)
-        # print np.min(x), np.max(x), np.count_nonzero(x < 1)
-        b = np.linalg.norm(pts2_norm - trans, axis=1)
         return np.linalg.norm(pts2_norm - trans, axis=1)

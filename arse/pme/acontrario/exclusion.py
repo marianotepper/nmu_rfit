8b7ef2b5ef605a08a5120d82ec21f4c75710d4da
marianotepper <Lulita75>
2016-03-14 10:45:38 -0400

    Assumes biclusters already are in the "correct" order, ,simplified code

--- arse/pme/acontrario/exclusion.py
+++ arse/pme/acontrario/exclusion.py
@@ -1,26 +1,19 @@
 import numpy as np
-import operator
 
 
 def exclusion_principle(x, thresholder, ac_tester, inliers_list, model_list):
-    nfa_list = [ac_tester.nfa(inliers, mod.min_sample_size)
-                for inliers, mod in zip(inliers_list, model_list)]
-    nfa_list = list(enumerate(nfa_list))
-    nfa_list = filter(lambda e: e[1] < ac_tester.epsilon, nfa_list)
-    nfa_list = sorted(nfa_list, key=operator.itemgetter(1))
-    candidates = zip(*nfa_list)[0]
-
     keep_list = []
-    for cand in candidates:
-        cand_mod = model_list[cand]
-        in_list = [np.nan_to_num(inliers_list[other]) for other in keep_list]
-        if not in_list:
-            keep_list.append(cand)
+    excluded = None
+    for k, (inliers, mod) in enumerate(zip(inliers_list, model_list)):
+        if excluded is None:
+            keep_list.append(k)
+            excluded = inliers > 0
             continue
-        excluded = reduce(lambda x, y: np.logical_or(x, y), in_list)
-        considered = np.logical_not(excluded)
 
-        membership = thresholder.membership(cand_mod, x[considered])
-        if ac_tester.meaningful(membership, cand_mod.min_sample_size):
-            keep_list.append(cand)
+        considered = np.logical_not(excluded)
+        membership = thresholder.membership(mod, x[considered])
+        if ac_tester.meaningful(membership):
+            keep_list.append(k)
+            excluded = np.logical_or(excluded, inliers > 0)
+
     return keep_list

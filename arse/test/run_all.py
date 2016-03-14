fab7a3e7da16da92a5850017fdbc69fc5d794c77
marianotepper <Lulita75>
2016-03-14 13:25:38 -0400

    Update run_all script

--- arse/test/run_all.py
+++ arse/test/run_all.py
@@ -2,11 +2,11 @@ import test_2d
 import test_vp
 import test_pozzoveggiani
 import test_piazza_bra
-import test_homography
+import test_transformations
 
 test_2d.run_all()
 test_vp.run_all()
 test_pozzoveggiani.run_all()
 test_piazza_bra.run_all()
-test_homography.run_all()
+test_transformations.run_all()
 

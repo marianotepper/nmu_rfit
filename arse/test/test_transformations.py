8b76d300f1f7d2eb6eb9ebc986e94c20466b2054
marianotepper <Lulita75>
2016-03-14 12:58:25 -0400

    Removed adaptive stuff, test fully functional

--- arse/test/test_transformations.py
+++ arse/test/test_transformations.py
@@ -14,7 +14,9 @@ import arse.biclustering as bc
 import arse.test.utils as test_utils
 import arse.pme.preference as pref
 import arse.pme.multigs as multigs
+import arse.pme.membership as membership
 import arse.pme.homography as homography
+import arse.pme.fundamental as fundamental
 import arse.pme.acontrario as ac
 
 
@@ -31,10 +33,13 @@ def load(path, tol=1e-5):
     gt = gt[mask]
     x = x[mask, :]
 
-    # sort in reverse order (inliers first, ourliers last)
+    # sort in reverse order (inliers first, outliers last)
     inv_order = np.argsort(gt)[::-1]
     gt = gt[inv_order]
     x = x[inv_order, :]
+
+    x[:, 0:2] -= np.array(data['img1'].shape[:2], dtype=np.float) / 2
+    x[:, 3:5] -= np.array(data['img2'].shape[:2], dtype=np.float) / 2
 
     data['data'] = x
     data['label'] = gt
@@ -43,10 +48,11 @@ def load(path, tol=1e-5):
 
 def base_plot(data):
     def inner_plot_img(pos, img):
+        pos_rc = pos + np.array(img.shape[:2], dtype=np.float) / 2
         gray_image = PIL.Image.fromarray(img).convert('L')
         plt.hold(True)
         plt.imshow(gray_image, cmap='gray')
-        plt.scatter(pos[:, 0], pos[:, 1], c='w', marker='o', s=10)
+        plt.scatter(pos_rc[:, 0], pos_rc[:, 1], c='w', marker='o', s=10)
         plt.axis('off')
 
     x = data['data']
@@ -59,12 +65,23 @@ def base_plot(data):
 
 def plot_models(data, groups, palette, s=10, marker='o'):
     def inner_plot_img(pos, img):
+        pos_rc = pos + np.array(img.shape[:2], dtype=np.float) / 2
         plt.hold(True)
         gray_image = PIL.Image.fromarray(img).convert('L')
-        plt.imshow(gray_image, cmap='gray')
+        plt.imshow(gray_image, cmap='gray', interpolation='none')
         for g, color in zip(groups, palette):
-            plt.scatter(pos[g, 0], pos[g, 1], c=color, edgecolors='face',
+            plt.scatter(pos_rc[g, 0], pos_rc[g, 1], c=color, edgecolors='face',
                         marker=marker, s=s)
+
+        # labels = ['{0}'.format(i) for i in range(pos.shape[0])]
+        # for label, x, y in zip(labels, pos_rc[:, 0], pos_rc[:, 1]):
+        #     plt.annotate(label, xy=(x, y), xytext=(-10, 10), size=3,
+        #                  textcoords='offset points', ha='right', va='bottom',
+        #                  bbox=dict(boxstyle='round, pad=0.5', fc='yellow',
+        #                            alpha=0.5),
+        #                  arrowprops=dict(arrowstyle='->', linewidth=.5,
+        #                                  color='yellow',
+        #                                  connectionstyle='arc3,rad=0'))
         plt.axis('off')
 
     x = data['data']
@@ -85,15 +102,15 @@ def ground_truth(labels):
     return gt_groups
 
 
-def run_biclustering(model_class, data, original_models, pref_matrix, comp_level,
+def run_biclustering(model_class, data, pref_matrix, comp_level, thresholder,
                      ac_tester, output_prefix, palette='Set1'):
     t = timeit.default_timer()
     bic_list = bc.bicluster(pref_matrix, comp_level=comp_level)
     t1 = timeit.default_timer() - t
     print('Time:', t1)
 
-    models, bic_list = test_utils.clean(model_class, data['data'], ac_tester,
-                                        bic_list)
+    bic_list = test_utils.clean(model_class, data['data'], thresholder,
+                                ac_tester, bic_list, share_elements=False)[1]
 
     colors = sns.color_palette(palette, len(bic_list))
 
@@ -106,7 +123,7 @@ def run_biclustering(model_class, data, original_models, pref_matrix, comp_level
     plot_models(data, bc_groups, palette=colors)
     plt.savefig(output_prefix + '_final_models.pdf', dpi=600)
 
-    inliers = reduce(lambda x, y: np.logical_or(x, y), bc_groups)
+    inliers = reduce(lambda a, b: np.logical_or(a, b), bc_groups)
     bc_groups.append(np.logical_not(inliers))
     gt_groups = ground_truth(data['label'])
     gnmi, prec, rec = test_utils.compute_measures(gt_groups, bc_groups)
@@ -114,11 +131,12 @@ def run_biclustering(model_class, data, original_models, pref_matrix, comp_level
     return dict(time=t1, gnmi=gnmi, precision=prec, recall=rec)
 
 
-def test(model_class, data, name, ransac_gen, ac_tester):
+def test(model_class, data, name, ransac_gen, thresholder, ac_tester,
+         dir_name):
     x = data['data']
     print(name, x.shape)
 
-    output_dir = '../results/homography/'
+    output_dir = '../results/{0}/'.format(dir_name)
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
     output_prefix = output_dir + name
@@ -134,17 +152,14 @@ def test(model_class, data, name, ransac_gen, ac_tester):
     plot_models(data, gt_groups, palette=gt_colors, s=.1, marker='.')
     plt.savefig(output_prefix + '_gt1.pdf', dpi=600)
 
-    pref_matrix, orig_models = pref.build_preference_matrix(x.shape[0],
-                                                            ransac_gen,
-                                                            ac_tester)
+    pref_matrix, _ = pref.build_preference_matrix(ransac_gen, thresholder,
+                                                  ac_tester)
 
     scipy.io.savemat(output_prefix + '.mat', {'pref_matrix': pref_matrix})
     with open(output_prefix + '.pickle', 'wb') as handle:
         pickle.dump(pref_matrix, handle)
-        pickle.dump(orig_models, handle)
     with open(output_prefix + '.pickle', 'rb') as handle:
         pref_matrix = pickle.load(handle)
-        orig_models = pickle.load(handle)
 
     print('Preference matrix size:', pref_matrix.shape)
 
@@ -154,27 +169,28 @@ def test(model_class, data, name, ransac_gen, ac_tester):
 
     print('Running regular bi-clustering')
     compression_level = None
-    stats_reg = run_biclustering(model_class, data, orig_models, pref_matrix,
-                                 compression_level, ac_tester,
+    stats_reg = run_biclustering(model_class, data, pref_matrix,
+                                 compression_level, thresholder, ac_tester,
                                  output_prefix + '_bic_reg')
 
     print('Running compressed bi-clustering')
     compression_level = 32
-    stats_comp = run_biclustering(model_class, data, orig_models, pref_matrix,
-                                  compression_level, ac_tester,
+    stats_comp = run_biclustering(model_class, data, pref_matrix,
+                                  compression_level, thresholder, ac_tester,
                                   output_prefix + '_bic_comp')
 
     return stats_reg, stats_comp
 
 
-def run_all():
-    logger = test_utils.Logger("test_homography.txt")
+def run(transformation, inliers_threshold):
+    logger = test_utils.Logger('test_{0}_{1:.0e}.txt'.format(transformation,
+                                                             inliers_threshold))
     sys.stdout = logger
 
-    inliers_threshold = 3.
+    n_samples = 2000
     epsilon = 0
 
-    path = '../data/adelaidermf/homography/'
+    path = '../data/adelaidermf/{0}/'.format(transformation)
 
     filenames = []
     for (_, _, fn) in os.walk(path):
@@ -183,12 +199,29 @@ def run_all():
 
     stats_list = []
     for example in filenames:
-        data = load(path + example)
-        x = data['data']
+        # if example != 'biscuit.mat':
+        #     continue
+        # if example != 'biscuitbookbox.mat':
+        #     continue
+        # if example != 'breadcartoychips.mat':
+        #     continue
+        # if example != 'boardgame.mat':
+        #     continue
 
-        n_samples = 2000
-        generator = multigs.ModelGenerator(homography.Homography, x, n_samples)
-        ac_tester = ac.LocalNFA(x, epsilon, inliers_threshold, ratio=30)
+        data = load(path + example)
+
+        if transformation == 'homography':
+            model_class = homography.Homography
+            nfa_proba = 0
+        else:
+            model_class = fundamental.Fundamental
+            nfa_proba = (2 * np.linalg.norm(data['img1'].shape) /
+                         np.prod(data['img1'].shape))
+
+        generator = multigs.ModelGenerator(model_class, data['data'], n_samples)
+        min_sample_size = model_class().min_sample_size
+        ac_tester = ac.ImageTransformNFA(epsilon, nfa_proba, min_sample_size)
+        thresholder = membership.GlobalThresholder(inliers_threshold)
 
         seed = 0
         # seed = np.random.randint(0, np.iinfo(np.uint32).max)
@@ -196,12 +229,14 @@ def run_all():
         np.random.seed(seed)
 
         prefix = example[:-4]
-        res = test(homography.Homography, data, prefix, generator, ac_tester)
+        dir_name = '{0}_{1:.0e}'.format(transformation, inliers_threshold)
+
+        res = test(model_class, data, prefix, generator, thresholder, ac_tester,
+                   dir_name)
         stats_list.append(res)
 
         print('-'*40)
-        # plt.close('all')
-        # break
+        plt.close('all')
 
     reg_list, comp_list = zip(*stats_list)
 
@@ -214,6 +249,12 @@ def run_all():
     sys.stdout = logger.stdout
     logger.close()
 
+
+def run_all():
+    # run('homography', 3.)
+    for thresh in np.arange(2.5e-3, 2.51e-2, 2.5e-3):
+        print(thresh)
+        # run('fundamental', thresh)
 
 if __name__ == '__main__':
     run_all()

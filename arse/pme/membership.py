02726fd9e113ec65f64db063a54701b8aece39d9
marianotepper <Lulita75>
2016-03-14 12:53:23 -0400

    Adaptive memberships removed

--- arse/pme/membership.py
+++ arse/pme/membership.py
@@ -20,53 +20,6 @@ class LocalThresholder(object):
                                 self.ratio)
 
 
-class AdaptiveThresholder(object):
-    __metaclass__ = ABCMeta
-
-    def __init__(self, tester, threshold_list):
-        self.tester = tester
-        self.threshold_list = threshold_list
-
-    def membership(self, model, data):
-        distances = model.distances(data)
-
-        best_nfa = self.tester.epsilon
-        best_membership = None
-        for thresh in self.threshold_list:
-            membership = self._inner_membership(distances, thresh)
-            nfa = self.tester.nfa(membership)
-            if nfa < best_nfa:
-                best_nfa = nfa
-                best_membership = membership
-
-        if best_membership is None:
-            best_membership = distances.copy()
-            best_membership[:] = np.nan
-
-        return best_membership
-
-    @abstractmethod
-    def _inner_membership(self, distances, inliers_threshold):
-        pass
-
-
-class AdaptiveGlobalThresholder(AdaptiveThresholder):
-    def __init__(self, tester, threshold_list):
-        super(AdaptiveGlobalThresholder, self).__init__(tester, threshold_list)
-
-    def _inner_membership(self, distances, inliers_threshold):
-        return global_membership(distances, inliers_threshold)
-
-
-class AdaptiveLocalThresholder(object):
-    def __init__(self, tester, threshold_list, ratio=3.):
-        super(AdaptiveGlobalThresholder, self).__init__(tester, threshold_list)
-        self.ratio = ratio
-
-    def _inner_membership(self, distances, inliers_threshold):
-        return local_membership(distances, inliers_threshold, self.ratio)
-
-
 def global_membership(distances, inliers_threshold):
     return (distances <= inliers_threshold) * inliers_threshold
 
